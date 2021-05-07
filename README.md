c0956adcdfa4c0765d473162653eadab60bbe3ec
succeun <succeun@gmail.com>
2021-05-08 00:30:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# mermaid
+View your mermaid code examples with mermaid.js for Typemill
+
+## snapshot image
+![snapshot](snapshot.png)
---
+++ mermaid.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Plugins\Mermaid;
+
+use \Typemill\Plugin;
+
+class Mermaid extends Plugin
+{
+	protected $settings;
+	
+    public static function getSubscribedEvents()
+    {
+		return array(
+			'onSettingsLoaded'		=> 'onSettingsLoaded',		
+			'onTwigLoaded' 			=> 'onTwigLoaded'
+		);
+    }
+	
+	public function onSettingsLoaded($settings)
+	{
+		$this->settings = $settings->getData();
+	}
+	
+	
+	public function onTwigLoaded()
+	{
+		$mermaidSettings = $this->settings['settings']['plugins']['mermaid'];
+		
+		if (isset($mermaidSettings['theme'])) {
+			$theme = $mermaidSettings['theme'];
+		} else {
+			$theme = 'default';
+		}
+		
+		if (isset($mermaidSettings['securityLevel'])) {
+			$securityLevel = $mermaidSettings['securityLevel'];
+		} else {
+			$securityLevel = 'strict';
+		}
+		
+		if (isset($mermaidSettings['htmlLabels'])) {
+			$htmlLabels = $mermaidSettings['htmlLabels'];
+		} else {
+			$htmlLabels = 'true';
+		}
+		
+		if (isset($mermaidSettings['fontFamily'])) {
+			$fontFamily = $mermaidSettings['fontFamily'];
+		} else {
+			$fontFamily = '';
+		}
+		
+		$this->addJS('/mermaid/public/mermaid.min.js');
+		
+		$this->addInlineJS("mermaid.initialize({'theme': '".$theme."', 'securityLevel': '".$securityLevel."', 'htmlLabels': ".$htmlLabels.", 'fontFamily': '".$fontFamily."'});");
+	
+		/* initialize the script */
+		$this->addInlineJS(<<<STR_END
+		document.addEventListener("DOMContentLoaded", function() {
+			document.querySelectorAll('code.language-mermaid').forEach(function(element, index) {
+				var content = element.innerHTML.replace(/&amp;/g, '&');
+				tempDiv = document.createElement('div');
+				tempDiv.className = "mermaid";
+				tempDiv.align = "center";
+				tempDiv.innerHTML = content;
+				element.parentNode.parentNode.replaceChild(tempDiv, element.parentNode);
+			});
+		});
+		STR_END);
+	}
+}
